9af14c6015d86eb1d64b278a207cb4b9d3f65ff8
DigiFenix917 <bryonmascher.cybersec.2023@gmail.com>
2025-08-06 18:45:38 -0600

    feat: Rolling Dice with Plotly feat: 15-6 Two D8 feat: 15-7 Three Dice feat: 15-8 Multiplication feat: 15-9 Die Comprehensions feat: 15-10 Practicing with Both Libraries

---
+++ src/dice_visual_2d10_list_comp.py
@@ -0,0 +1,27 @@
+# Exercise 15.6: Two D8s
+import plotly.express as px
+
+from die import Die
+
+# Create two D6.
+die_1 = Die(10)
+die_2 = Die(10)
+
+# Make some rolls.
+results = [(die_1.roll() + die_2.roll()) for roll_num in range(1000)]
+
+# Analyze the results.
+max_result = die_1.num_sides + die_2.num_sides
+poss_results = range(2, max_result+1)
+frequencies = [results.count(value) for value in poss_results]
+
+# Visualize the results.
+title = "Results of Rolling two D10 Dice 1,000 Times."
+labels = {'x': 'Result', 'y': 'Frequency of Result'}
+fig = px.bar(x=poss_results, y=frequencies, title=title, labels=labels)
+
+# Further customize chart.
+fig.update_layout(xaxis_dtick=1)
+
+fig.show()
+# fig.write_html('../html/dice_visual_2d8.html', '')
---
+++ src/rw_plotly.py
@@ -0,0 +1,24 @@
+# Exercise 15.10: Practicing with Both Libraries (Plotly Random Walk)
+import plotly.graph_objects as go
+import numpy as np
+
+l = 1000
+x_steps = np.random.choice([-1, 1], size=l) + 0.2 * np.random.randn(l) # l steps
+y_steps = np.random.choice([-1, 1], size=l) + 0.2 * np.random.randn(l) # l steps
+x_position = np.cumsum(x_steps) # Integrate the position by summing steps values
+y_position = np.cumsum(y_steps) # Integrate the position by summing steps values
+
+fig = go.Figure(data=go.Scatter(
+    x=x_position,
+    y=y_position,
+    mode='markers',
+    name='Random Walk',
+    marker=dict(
+         color=np.arange(l),
+        size=8,
+        colorscale='Greens',
+        showscale=True
+    )
+))
+
+fig.show()
